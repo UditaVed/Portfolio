b7cc4d2117da27deccbe5fb82a47618654106f19
u_ved <97843629+UditaVed@users.noreply.github.com>
2025-01-13 14:00:45 +0530

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -6,6 +6,8 @@ const Navbar = () => {
     <div className='flex md:flex-row-reverse md:justify-normal gap-6 justify-center text-sm md:text-[16px]'>
         <Link href={Data.contacts[0].link} target='_blank' className='hover:text-black'>{Data.contacts[0].name}</Link>
         <Link href={Data.contacts[1].link} target='_blank' className='hover:text-black'>{Data.contacts[1].name}</Link>
+        <Link href={Data.contacts[2].link} target='_blank' className='hover:text-black'>{Data.contacts[2].name}</Link>
+        <Link href={Data.contacts[3].link} target='_blank' className='hover:text-black'>{Data.contacts[3].name}</Link>
     </div>
   )
 }
